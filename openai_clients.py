6b4f780ff5fa66ec7b2c6d21c2d8345773e51add
Salih <134608297+SalihBirdal2434@users.noreply.github.com>
2025-07-08 21:37:34 +0300

    Update openai_clients.py

--- openai_clients.py
+++ openai_clients.py
@@ -2,17 +2,12 @@ from langchain.chat_models import init_chat_model
 from langchain_openai import OpenAIEmbeddings
 import os
 from dotenv import load_dotenv
-from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()  # .env dosyasını yükle
 
 llm = init_chat_model(
-    model="gpt-4o",                    
+    model="gpt-4o",                     # veya "gpt-4o-mini" çalışıyorsa
     model_provider="openai",
-    api_key=os.getenv("OPENAI_API_KEY")  
+    api_key=os.getenv("OPENAI_API_KEY")  # ✅ DOĞRU kullanım bu!
 )
-embedding_model = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-m3",
-    model_kwargs={"device": "cpu"},  # GPU varsa "cuda" yaz
-    encode_kwargs={"normalize_embeddings": True}  # cosine similarity için çok önemli
-)
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large",api_key=os.getenv("OPENAI_API_KEY"))
